808bf63628eda142fe449fddd3e10637dba19175
DeepakV077 <deepakv30082006@gmail.com>
2025-07-03 19:35:16 +0530

    Changes made

---
+++ script.js
@@ -0,0 +1,19 @@
+function redButton() {
+    document.body.style.backgroundColor = "red";
+    document.getElementById("colorDisplay").innerHTML = "Current Color: Red";
+}
+
+function greenButton() {
+    document.body.style.backgroundColor = "green";
+    document.getElementById("colorDisplay").innerHTML = "Current Color: Green";
+}
+
+function blueButton() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("colorDisplay").innerHTML = "Current Color: Blue";
+}
+
+function whiteButton() {
+    document.body.style.backgroundColor = "white";
+    document.getElementById("colorDisplay").innerHTML = "Current Color: White";
+}
